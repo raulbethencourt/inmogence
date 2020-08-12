42ff22c54238285a17a0ae92d57980b699f4011e
Raul <raul.bethencourt.pro@gmail.com>
2020-08-12 16:17:49 +0200

    :construction: I work adding my Properties

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,15 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager as PersistenceObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;     
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * PropertyController class
  */
 class PropertyController extends AbstractController
 {
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, PersistenceObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
     /**
      * @Route("/biens", name="property.index")
      * @return Response
